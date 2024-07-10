50a40fd01ed935a6c35114018f5684143868f5b8
Med Amine Chakroun <chakrounmedamine19@gmail.com>
2024-07-10 14:55:47 +0100

    Calling API

--- foodApp/src/components/Search.jsx
+++ foodApp/src/components/Search.jsx
@@ -1,7 +1,17 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
+const URL = "https://api.spoonacular.com/recipes/complexSearch";
+const API_KEY = "6b0655a8fa7a47f8a7923f85996e9b82";
 export default function Search() {
   const [query, setQuery] = useState("");
+  useEffect(() => {
+    async function fetchFood() {
+      const res = await fetch(`${URL}?query=${query}&apiKey=${API_KEY}`);
+      const data = await res.json();
+      console.log(data);
+    }
+    fetchFood();
+  }, [query]);
   return (
     <div>
       <input
