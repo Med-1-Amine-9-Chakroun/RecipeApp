717ff8699393eeb4e9795993d8bd096dd1d0deb8
Med Amine Chakroun <chakrounmedamine19@gmail.com>
2024-07-10 15:31:08 +0100

    Item food and List food components created

---
+++ foodApp/src/components/FoodItem.jsx
@@ -0,0 +1,7 @@
+export default function FoodItem({ food }) {
+  return (
+    <div>
+      <h1>{food.title}</h1>
+    </div>
+  );
+}
--- foodApp/src/components/Search.jsx
+++ foodApp/src/components/Search.jsx
@@ -2,13 +2,14 @@ import { useEffect, useState } from "react";
 
 const URL = "https://api.spoonacular.com/recipes/complexSearch";
 const API_KEY = "6b0655a8fa7a47f8a7923f85996e9b82";
-export default function Search() {
+export default function Search({ foodData, setFoodData }) {
   const [query, setQuery] = useState("");
   useEffect(() => {
     async function fetchFood() {
       const res = await fetch(`${URL}?query=${query}&apiKey=${API_KEY}`);
       const data = await res.json();
       console.log(data);
+      setFoodData(data.results);
     }
     fetchFood();
   }, [query]);
