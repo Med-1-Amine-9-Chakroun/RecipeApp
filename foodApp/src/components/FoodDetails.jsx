7727b78120fa55093f4c7036fffcdee440f81f17
Med Amine Chakroun <chakrounmedamine19@gmail.com>
2024-07-11 10:10:25 +0100

    Fetching recipes from API

--- foodApp/src/components/FoodDetails.jsx
+++ foodApp/src/components/FoodDetails.jsx
@@ -1,3 +1,22 @@
+import { useEffect, useState } from "react";
+
 export default function FoodDetails({ foodId }) {
-  return <div>{foodId}</div>;
+  const [food, setFood] = useState({});
+  const URL = `https://api.spoonacular.com/recipes/${foodId}/information`;
+  const API_KEY = "6b0655a8fa7a47f8a7923f85996e9b82";
+  useEffect(() => {
+    async function fetchFood() {
+      const res = await fetch(`${URL}?apiKey=${API_KEY}`);
+      const data = await res.json();
+      console.log(data);
+      setFood(data);
+    }
+    fetchFood();
+  }, [foodId]);
+  return (
+    <div>
+      {foodId}
+      {food.title}
+    </div>
+  );
 }
